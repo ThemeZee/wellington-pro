a789d9855553d30bebb171d256600146b640706c
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-08-12 11:58:24 +0200

    changed implementation of top navigation menu

--- includes/modules/class-header-bar.php
+++ includes/modules/class-header-bar.php
@@ -8,7 +8,9 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Header Bar Class
@@ -42,7 +44,7 @@ class Wellington_Pro_Header_Bar {
 		// Check if there are menus.
 		if ( has_nav_menu( 'social' ) or has_nav_menu( 'secondary' ) ) {
 
-			echo '<div id="header-bar" class="header-bar clearfix">';
+			echo '<div id="header-bar" class="header-bar">';
 
 			// Check if there is a social menu.
 			if ( has_nav_menu( 'social' ) ) {
@@ -52,38 +54,50 @@ class Wellington_Pro_Header_Bar {
 				// Display Social Icons Menu.
 				wp_nav_menu( array(
 					'theme_location' => 'social',
-					'container' => false,
-					'menu_class' => 'social-icons-menu',
-					'echo' => true,
-					'fallback_cb' => '',
-					'link_before' => '<span class="screen-reader-text">',
-					'link_after' => '</span>',
-					'depth' => 1,
-					)
-				);
+					'container'      => false,
+					'menu_class'     => 'social-icons-menu',
+					'echo'           => true,
+					'fallback_cb'    => '',
+					'link_before'    => '<span class = "screen-reader-text">',
+					'link_after'     => '</span>',
+					'depth'          => 1,
+				) );
 
 				echo '</div>';
 
 			}
 
 			// Check if there is a top navigation menu.
-			if ( has_nav_menu( 'secondary' ) ) {
+			if ( has_nav_menu( 'secondary' ) ) : ?>
 
-				echo '<nav id="top-navigation" class="secondary-navigation navigation clearfix" role="navigation">';
+				<button class="secondary-menu-toggle menu-toggle" aria-controls="secondary-menu" aria-expanded="false">
+					<?php
+					echo wellington_get_svg( 'menu' );
+					echo wellington_get_svg( 'close' );
+					?>
+					<span class="menu-toggle-text screen-reader-text"><?php esc_html_e( 'Navigation', 'wellington-pro' ); ?></span>
+				</button>
 
-				// Display Top Navigation.
-				wp_nav_menu( array(
-					'theme_location' => 'secondary',
-					'container' => false,
-					'menu_class' => 'top-navigation-menu',
-					'echo' => true,
-					'fallback_cb' => '',
-					)
-				);
+				<div class="secondary-navigation">
 
-				echo '</nav>';
+					<nav class="top-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Secondary Menu', 'wellington-pro' ); ?>">
 
-			}
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'secondary',
+								'menu_id'        => 'secondary-menu',
+								'container'      => false,
+							)
+						);
+						?>
+
+					</nav>
+
+				</div><!-- .secondary-navigation -->
+
+				<?php
+			endif;
 
 			echo '</div>';
 		}
@@ -103,7 +117,7 @@ class Wellington_Pro_Header_Bar {
 
 		register_nav_menus( array(
 			'secondary' => esc_html__( 'Top Navigation', 'wellington-pro' ),
-			'social' => esc_html__( 'Social Icons', 'wellington-pro' ),
+			'social'    => esc_html__( 'Social Icons', 'wellington-pro' ),
 		) );
 
 	}
