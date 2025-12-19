ed01f8f3e78be4ac8e3b7c4ec4075c7f37a0b786
Thomas Weichselbaumer <t.weichselbaumer@inpsyde.com>
2025-12-19 20:29:40 +0100

    Add Bluesky to social icons menu

--- includes/modules/class-header-bar.php
+++ includes/modules/class-header-bar.php
@@ -165,7 +165,7 @@ class Wellington_Pro_Header_Bar {
 
 		// Create SVG markup.
 		$svg  = '<svg class="icon icon-' . esc_attr( $icon ) . '" aria-hidden="true" role="img">';
-		$svg .= ' <use xlink:href="' . WELLINGTON_PRO_PLUGIN_URL . 'assets/icons/social-icons.svg?ver=20240124#icon-' . esc_html( $icon ) . '"></use> ';
+		$svg .= ' <use xlink:href="' . WELLINGTON_PRO_PLUGIN_URL . 'assets/icons/social-icons.svg?ver=20251219#icon-' . esc_html( $icon ) . '"></use> ';
 		$svg .= '</svg>';
 
 		return $svg;
@@ -185,6 +185,7 @@ class Wellington_Pro_Header_Bar {
 			'bandcamp'        => 'bandcamp',
 			'behance.net'     => 'behance',
 			'bitbucket'       => 'bitbucket',
+			'bsky.app'        => 'bluesky',
 			'codepen'         => 'codepen',
 			'deviantart'      => 'deviantart',
 			'digg.com'        => 'digg',
